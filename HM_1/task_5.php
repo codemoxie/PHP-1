8bca16ebfabe7d15488e7ad3393a628548fa2b4a
Evgenia Pazukhina <67636349+codemoxie@users.noreply.github.com>
2020-10-29 14:26:20 +0300

    Add files via upload
    first homework

---
+++ HM_1/task_5.php
@@ -0,0 +1,13 @@
+<?php
+
+$first = 20;
+$second = 30;
+
+$first = $first + $second;    // $first = 50
+$second = $first - $second;    // $second = 20
+$first = $first - $second;    // $first = 30
+
+echo 'Здесь было 20, а теперь ' . $first;
+echo 'Здесь было 30, а теперь ' . $second;
+
+?>
